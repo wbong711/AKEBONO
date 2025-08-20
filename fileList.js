a8a72eb0b501f9a32a96f89c41d6304b120d649d
Kay <116565013+wbong711@users.noreply.github.com>
2025-08-20 13:03:28 +0900

    Create fileList.js

---
+++ fileList.js
@@ -0,0 +1,10 @@
+// fileList.js 파일 내용
+
+const filesToDisplay = [
+    "page1.html",
+    "page2.html",
+    "images/photo.png",
+    "docs/manual.pdf",
+    // 여기에 필요한 다른 파일 경로들을 추가하거나 제거해줘!
+    "notes/summary.txt"
+];
